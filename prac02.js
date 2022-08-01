b162e41a04aba940ced9e04085bf888a64ff570d
Muhammad Shakir <84896803+MuhammadShakir-dev@users.noreply.github.com>
2022-08-01 13:28:50 +0500

    Update prac02.js

--- prac02.js
+++ prac02.js
@@ -1,1 +1,11 @@
+// spread in function .
 
+const ary = [10,90,89,67,56,45,786,564,987,765,654,987,253];
+
+console.log(ary);
+
+const max = Math.max(...ary);
+console.log("The maximum number is  : " + max);
+
+const min = Math.min(...ary);
+console.log("The Minimum number is  :  " +  min);
