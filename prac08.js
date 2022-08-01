aeee79e7960e262079a1c917f67268e692ed898b
Muhammad Shakir <84896803+MuhammadShakir-dev@users.noreply.github.com>
2022-08-02 00:21:26 +0500

    Update prac08.js

--- prac08.js
+++ prac08.js
@@ -1,2 +1,20 @@
 // let's have a look on parameters destructuring in javascript
 
+const uniDetails = {
+    name : "SMIU",
+    location : "Karachi",
+    certified : "HEC",
+    programs : "BS(SE,CS,IT,DATA-SCIENCE,CYBER-SECURITY,AI) & Non BS",
+    telNO : 0213471882,
+    sector : "Public",
+};
+
+function uni(uniDetails){
+    const {name,location,sector,telNo} = uniDetails;
+    return `${name} ${location} ${sector}`;
+};
+
+uni(uniDetails);
+
+
+
