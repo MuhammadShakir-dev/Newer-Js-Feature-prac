117caaf5889fffc58cdc4b4ac24b1cb0d2247c1f
Muhammad Shakir <84896803+MuhammadShakir-dev@users.noreply.github.com>
2022-08-01 16:16:53 +0500

    Update prac04.js

--- prac04.js
+++ prac04.js
@@ -1,1 +1,24 @@
+// lets practice some spread in objects in javascript .
 
+const studentInfo = {
+    name : "Muhammad Shakir",
+    age : 21,
+    gender : "Male",
+    status : "Student",
+};
+
+const acadamicDetails = {
+    university : "SMIU",
+    semester : 5,
+    gpa : 3.2, 
+};
+
+const residence = {
+    city : "Karachi",
+    country : "Pakistan",
+};
+
+
+const merjAllData = {...studentInfo, ...acadamicDetails, ...residence};
+
+console.log(merjAllData);
