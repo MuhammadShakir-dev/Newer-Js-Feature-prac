a4e09b5e7ba9a02bc9987180f8b6a51eb19f4196
Muhammad Shakir <84896803+MuhammadShakir-dev@users.noreply.github.com>
2022-08-01 13:06:45 +0500

    Update prac01.js

--- prac01.js
+++ prac01.js
@@ -1,1 +1,23 @@
+// lets paractice some default parameters in javascript and what is the new methods ot syntax to do this.
+
+
+// Example no 1
+const fullName  =  function(firstname  = "Muhammad", lastname = "Ali"){
+    return console.log("Hi!" + " " + firstname + " " + lastname)
+}
+
+fullName("Muhammad","Shakir");
+
+
+// Example no 2
+const rollDice  = function(start = 6){
+    return Math.floor(Math.random() * start) + 1;
+} 
+
+rollDice();
+
+
+// Example no 3.
+
+
 
