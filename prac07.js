af00fa9ed2195e8cbbc7491586ed270068c15f21
Muhammad Shakir <84896803+MuhammadShakir-dev@users.noreply.github.com>
2022-08-02 00:10:17 +0500

    Update prac07.js

--- prac07.js
+++ prac07.js
@@ -1,1 +1,12 @@
+// let's have a look on destructring objects in javascript
 
+const uniDetails = {
+    name : "SMIU",
+    location : "Karachi",
+    certified : "HEC",
+    programs : "BS(SE,CS,IT,DATA-SCIENCE,CYBER-SECURITY,AI) & Non BS",
+    telNO : 0213471882,
+    sector : "Public"
+};
+
+const {name,location,certified,programs,telNo,sector} = uniDetails;
